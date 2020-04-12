a564ccc0c85f07883608d06f8e4909250d6b8ea8
awaclawczykatpo <61697938+awaclawczykatpo@users.noreply.github.com>
2020-04-12 14:13:50 +0200

    Add files via upload

--- ajaxrequest.js
+++ ajaxrequest.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
-  $("button").click(function(){
-    $.getJSON("getsthviaajax.js", function(result){
-      console.log("test");
-        $("#figs").append(result + " ");
-      
-    });
-  });
-});
+function loadDoc(url) {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("#figs").append(this.responseText);
+  }
+};
+  xhttp.open("GET", url, true);
+  xhttp.send();
+}
